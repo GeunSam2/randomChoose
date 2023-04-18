08879420929f2a893d11968ddce401b335125c7d
GeunSam2 <rootiron96@gmail.com>
2023-04-18 09:44:51 +0900

    제목 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,7 +88,7 @@ export default function RandomChoose() {
           </svg>
         </div>
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">두근두근 랜덤뽑기</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">숫자 랜덤 뽑기</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600 font-nanumBGothic">
             숫자 뽑기를 통해 랜덤으로 당첨자들을 뽑아보세요.
           </p>
